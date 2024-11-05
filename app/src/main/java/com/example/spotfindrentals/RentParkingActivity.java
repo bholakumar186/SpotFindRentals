e4807beeadbd2ea4a442b2064853c0e7266ed274
Bhola <bhola186>
2024-11-06 01:02:07 +0530

    Second

--- app/src/main/java/com/example/spotfindrentals/RentParkingActivity.java
+++ app/src/main/java/com/example/spotfindrentals/RentParkingActivity.java
@@ -24,7 +24,6 @@ import androidx.core.view.WindowInsetsCompat;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.messaging.FirebaseMessaging;
 
 public class RentParkingActivity extends AppCompatActivity {
 
